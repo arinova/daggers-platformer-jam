34bc0aabd990e757dd08e2cbaa74a383206df05d
Ben B <benbasuni1@gmail.com>
2023-11-03 20:38:31 -0700

    organized the names of the prefabs and working on respawning the enemies

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
+    public GameObject prefabToRespawn; // The prefab you want to respawn
+    public float respawnTime = 5f; // Time in seconds after which the prefab will respawn
+    public string prefabTag = "RespawnablePrefab";
     public float Health { get; set; }
     [SerializeField] private int maxHealth = 1;
     [SerializeField] private bool alive = true;
@@ -12,6 +15,22 @@ public class Enemy : MonoBehaviour, IDamageable
     void Start()
     {
         Health = maxHealth;
+        StartCoroutine(RespawnCoroutine());
+    }
+
+    private IEnumerator RespawnCoroutine()
+    {
+        while (true) // Infinite loop to keep checking for respawn
+        {
+            yield return new WaitForSeconds(respawnTime); // Wait for the specified time
+
+            // Check if there is already a prefab in the scene
+            GameObject existingPrefab = GameObject.FindWithTag(prefabTag);
+            if (existingPrefab == null) // If there is no prefab, instantiate a new one
+            {
+                Instantiate(prefabToRespawn, transform.position, transform.rotation);
+            }
+        }
     }
 
     // Update is called once per frame
