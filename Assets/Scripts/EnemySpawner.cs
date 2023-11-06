c633f03927da0cdc4f43d6cafac2d1e371030990
Ariane Joy Nova <arianenova@gmail.com>
2023-11-06 15:55:13 -0500

    Spawner spawns different enemy types in rotation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab; // Assign this in the inspector
+    public List<GameObject> enemyPrefabs = new List<GameObject>(); // Assign this in the inspector
     public float spawnRate = 5f;
     public Vector2[] spawnLocations;
     public int maxEnemies = 100;
@@ -13,26 +13,32 @@ public class EnemySpawner : MonoBehaviour
 
     private float nextSpawnTime;
     private List<GameObject> enemies = new List<GameObject>();
+    private int enemyPrefabsIndex = 0;
 
     private void Update()
     {
         // Check if it's time to spawn an enemy
         if (Time.time >= nextSpawnTime && enemies.Count < maxEnemies)
         {
-            Debug.Log("spawn" + enemies.Count);
-            SpawnEnemy();
             nextSpawnTime = Time.time + spawnRate;
             Vector2 spawnLocation = new Vector2(Random.Range(minX, maxX), y);
-            Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+            SpawnEnemy(GetNextEnemyPrefab(), spawnLocation);
+            enemyPrefabsIndex++;
         }
 
         // Cleanup any null entries if enemies were destroyed
         enemies.RemoveAll(item => item == null);
     }
 
-    private void SpawnEnemy()
+    private void SpawnEnemy(GameObject enemyPrefab, Vector2 spawnLocation)
     {
-        var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        var newEnemy = Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
         enemies.Add(newEnemy);
+    }
+
+    private GameObject GetNextEnemyPrefab()
+    {
+        if (enemyPrefabsIndex == enemyPrefabs.Count) enemyPrefabsIndex = 0;
+        return enemyPrefabs[enemyPrefabsIndex];
     }
 }
